85b02f28d9110204a40df9f57faeb28610c1c022
Sukhrobjon [FE20] <126710684+LoganS03@users.noreply.github.com>
2023-07-20 12:27:04 +0500

    validation is complete

--- js/script.js
+++ js/script.js
@@ -1,37 +1,123 @@
-const form = document.getElementById('form');
+let { form } = document.forms
+let inputs = Object.values(form)
 
-function isCheckboxOrRadio(type) {
-	return ['checkbox', 'radio'].includes(type);
+let inpStatus = []
+
+let maskOptions = {
+  mask: '+{998}(00)000-00-00'
+};
+
+let mask = IMask(document.querySelector('#phone'), maskOptions);
+
+function showError(parentElement, msgElement, message){
+	msgElement.textContent = message
+	parentElement.classList.add('error')
 }
 
-function submitForm(e) {
-	e.preventDefault();
-	let canSubmit = [];
+function hideError(parentElement, msgElement){
+	msgElement.textContent = ""
+	parentElement.classList.remove('error')
+}
 
-	const fields = document.querySelectorAll('input, select');
-	let userData = {};
+function inpValidate(){
+	inputs.forEach((input, index) => {
+		if(input.type != "checkbox" && input.type != "submit"){
+			inpStatus.push(false)
+			
+			input.addEventListener('blur', (e) => {
+				let target = e.target
+				let formGroup = input.parentElement
+				let errorElement = input.nextElementSibling
 
-	fields.forEach(field => {
-		const { name, value, type, checked } = field;
+				if(target.value === ''){
+					showError(formGroup, errorElement, "To'ldirilishi shart")
+					inpStatus[index] = false
+				}else{
+					hideError(formGroup, errorElement)
+					inpStatus[index] = true
+				}
+			})
 
-		userData[name] = isCheckboxOrRadio(type) ? checked : value;
+			input.addEventListener("input", (e) => {
+				let target = e.target
+				let formGroup = input.parentElement
+				let errorElement = input.nextElementSibling
+
+				if(target.name === 'name'){
+					if(target.value.length <= 2){
+						showError(formGroup, errorElement, 'Ism 3ta harfdan oshishi kerak')
+						inpStatus[index] = false
+					}else{
+						hideError(formGroup, errorElement)
+						inpStatus[index] = true
+					}
+				}else if(target.name === 'email'){
+					if(!target.value.includes("@")){
+						showError(formGroup, errorElement, 'Emailda @ belgisi bo\'lishi kerak')
+						inpStatus[index] = false
+					}else{
+						hideError(formGroup, errorElement)
+						inpStatus[index] = true
+					}
+				}else if(target.name === 'phone'){
+					if(target.value.length !== 17){
+						showError(formGroup, errorElement, 'Raqam noto\'g\'ri')
+						inpStatus[index] = false
+					}else{
+						hideError(formGroup, errorElement)
+						inpStatus[index] = true
+					}
+				}
+			})
+		}
+		else if(input.type == "checkbox"){
+			inpStatus.push(false)
+
+			input.addEventListener("change", () => {
+				if(input.checked){
+					inpStatus[index] = true
+				}
+				else{
+					inpStatus[index] = false
+				}
+			})
+		}
 	});
-
-	for(let i = 0; i < Object.values(userData).length; i++){
-		if(Object.values(userData)[i].length == 0 || Object.values(userData) == false){
-			canSubmit.push(false)
-		}
-		else{
-			canSubmit.push(true)
-		}
-	}
-
-	if(canSubmit.includes(false)){
-		alert("Barcha sohalar to'ldirilgan bo'lishi kerak!")
-	}
-	else{
-		console.dir(userData)
-	}
 }
 
-form.addEventListener('submit', submitForm);
+function isEmpty(){
+	inputs.forEach((input, index) => {
+		if(input.type != "checkbox" && input.type != "submit"){
+			let formGroup = input.parentElement
+			let errorElement = input.nextElementSibling
+
+			if(input.value == ''){
+				showError(formGroup, errorElement, 'To\'ldirilishi shart')
+				inpStatus[index] == false
+			}
+			else{
+				hideError(formGroup, errorElement)
+				inpStatus[index] == true
+			}
+		}
+		else if(input.type == "checkbox"){
+			inpStatus[index] == false
+		}
+	})
+}
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault()
+	let formData = new FormData(form)
+	let values = Object.fromEntries(formData.entries())
+
+	console.log(inpStatus);
+	if(!inpStatus.includes(false)){
+		console.log(values)
+	}else{
+		// alert('Something is wrong!')
+		isEmpty()
+	}
+})
+
+inpValidate()
